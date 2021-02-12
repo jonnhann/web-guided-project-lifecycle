6082b3d1954636af24c9e451e70668e0b069ab85
Josh Jacobson <joshjacobson14@gmail.com>
2021-02-11 18:00:42 -0800

    lecture work-in-progress

--- doggos/src/components/doggo.js
+++ doggos/src/components/doggo.js
@@ -3,5 +3,11 @@ import axios from 'axios';
 import "../styles.css";
 
 class Doggo extends React.Component {
-    
+  render() {
+    return (
+      <>
+        <img className="dog-image" alt={`dog image ${index}`} width="200" key={doggo} src={doggo} />
+      </>
+    )
+  }
 }
