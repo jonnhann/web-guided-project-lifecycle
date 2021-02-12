0214281161a11e435cd7e06caa50ada5f4f78468
Josh Jacobson <joshjacobson14@gmail.com>
2021-02-11 17:51:49 -0800

    lecture work-in-progress

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -11,7 +11,12 @@ class App extends React.Component {
 
   componentDidMount() {
     axios.get("https://dog.ceo/api/breed/hound/images")
-      .then(res => console.log(res.data.message))
+      .then(res => { 
+        this.setState({
+          ...this.state,
+          doggos: res.data.message
+        })
+      })
       .catch(err => console.log(err))
   }
 
