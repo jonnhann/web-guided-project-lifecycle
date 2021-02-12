2dcd4c408de76c777b6475076975ccc9f24b807b
Josh Jacobson <joshjacobson14@gmail.com>
2021-02-11 18:13:30 -0800

    lecture work-in-progress

--- doggos/src/components/doggo.js
+++ doggos/src/components/doggo.js
@@ -7,7 +7,7 @@ class Doggo extends React.Component {
     // ^ equivalent to const imageUrl = this.props.imageUrl
     return (
       <>
-        <img alt={imageUrl} width="200" src={imageUrl} />
+        <img className="dog-image" alt={imageUrl} width="200" src={imageUrl} />
       </>
     )
   }
