5e6a82b8c5e0f545a2caa3bec7f0e9439ecc55c6
Josh Jacobson <joshjacobson14@gmail.com>
2021-02-11 17:03:37 -0800

    lecture work-in-progress

--- pokemon/src/components/Pokemon.js
+++ pokemon/src/components/Pokemon.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Pokemon(props) {
-  return (
-    <>
-      {props.pokemon.map(pokemon => (
-        <div key={pokemon.id} className="pokemon">
-          <img src={pokemon.img} alt={pokemon.name} />
-          <div>
-            <h3>{pokemon.name}</h3>
-            {pokemon.next_evolution &&
-              pokemon.next_evolution.map(e => <p key={e.num}>{e.name}</p>)}
+class Pokemon extends React.Component {
+  render() {
+    return (
+      <>
+        {props.pokemon.map(pokemon => (
+          <div key={pokemon.id} className="pokemon">
+            <img src={pokemon.img} alt={pokemon.name} />
+            <div>
+              <h3>{pokemon.name}</h3>
+              {pokemon.next_evolution &&
+                pokemon.next_evolution.map(e => <p key={e.num}>{e.name}</p>)}
+            </div>
           </div>
-        </div>
-      ))}
-    </>
-  );
+        ))}
+      </>
+    );
+  }
+
 }
 
 export default Pokemon;
