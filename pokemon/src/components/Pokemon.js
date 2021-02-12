bae4dad47706dc97953c79b5fb890b7b3a246fe7
Josh Jacobson <joshjacobson14@gmail.com>
2021-02-11 17:13:12 -0800

    lecture work-in-progress

--- pokemon/src/components/Pokemon.js
+++ pokemon/src/components/Pokemon.js
@@ -15,7 +15,7 @@ class Pokemon extends React.Component {
             <img src={pokemon.img} alt={pokemon.name} />
             <div>
               <h3>{pokemon.name}</h3>
-              {pokemon.next_evolution.map(e => <p key={e.num}>{e.name}</p>)}
+              {pokemon.next_evolution && pokemon.next_evolution.map(e => <p key={e.num}>{e.name}</p>)}
             </div>
           </div>
         ))}
